2196e7abfdf81a6cc65a3156aa564caa8cef4234
hagoromo2000 <revtm000126@gmail.com>
2023-08-20 11:26:58 +0900

    パラメタライズドテストと前後処理

---
+++ src/chapter2/group/group.test.ts
@@ -0,0 +1,40 @@
+// グループ1
+describe("before/after timing", () => {
+  // グループ1の前後処理
+  beforeAll(() => console.log("1 - beforeAll"));
+  afterAll(() => console.log("1 - afterAll"));
+  beforeEach(() => console.log("1 - beforeEach"));
+  afterEach(() => console.log("1 - afterEach"));
+  // グループ1のテスト1
+  test("", () => console.log("1 - test1"));
+  // グループ2
+  describe("Scoped / Nested block", () => {
+    // グループ2の前後処理
+    beforeAll(() => console.log("2 - beforeAll"));
+    afterAll(() => console.log("2 - afterAll"));
+    beforeEach(() => console.log("2 - beforeEach"));
+    afterEach(() => console.log("2 - afterEach"));
+    // グループ2のテスト1
+    test("", () => console.log("2 - test1"));
+    // グループ2のテスト2
+    test("", () => console.log("2 - test2"));
+  });
+});
+
+describe("グループ名", () => {
+  test("テストケース1", () => {
+    expect(true).toBe(true);
+  });
+  test("テストケース2", () => {
+    expect(true).toBe(true);
+  });
+  test("テストケース3", () => {
+    expect(true).toBe(true);
+  });
+  // 入れ子でグループを定義できる
+  describe("グループ名", () => {
+    test("テストケース", () => {
+      expect(true).toBe(true);
+    });
+  });
+});
